245d66d04de87d98d8335012fb261df5eab24212
Kenni <kenni.bawden@gmail.com>
2012-03-30 04:26:11 +1100

    Only a certain number of Memory Units will displayed at any time now

--- emu-display.js
+++ emu-display.js
@@ -25,6 +25,12 @@ function displayObject() {
 function MemoryScrollbar(memory) {
 	// Display properties
 	this.id = "memory_scrollbar";
+	// The max/min number of memory units that can be in range
+	this.max_units = 1000;
+	this.min_units = 24;
+	
+	// Boolean to determine if changing slider values
+	this.sliding = false;
 
 	// Data properties
 	this.memoryObject = memory;
@@ -58,12 +64,62 @@ MemoryScrollbar.method('addHighlight', function(start, end, style) {
 MemoryScrollbar.method('getRange', function() {
 	// Get slider values
 	var range = this.jqueryObj().slider("values");
-	
+
 	// Needs to be reversed since the slider has
 	// 0 at the bottom instead of the top
-	return [this.memoryObject.size-range[1], this.memoryObject.size-range[0]];
+	range = [this.memoryObject.size-range[1], this.memoryObject.size-range[0]];
+
+	// Make sure minimum is maintained	
+	if (range[1] - range[0] < this.min_units)
+		range[1] = range[0] + this.min_units;
+
+	return range;
 });
 
+// Method to set the range selected by the scrollbar
+MemoryScrollbar.method('setRange', function(start, end) {
+	// Needs to be reversed since the slider has
+	// 0 at the bottom instead of the top
+	var range = [this.memoryObject.size-end, this.memoryObject.size-start];
+
+	// Get slider values
+	var range = this.jqueryObj().slider("values", range);
+});
+
+// Gives the slider a maximum range
+// when a handle is pulled out of the maximum range
+// the other handle is pulled with it
+// Returns if the scroll bar is within the range
+MemoryScrollbar.method('maxRange', function(event, ui) {
+	// Ignore any attempts to restrict range if already doing so
+	if (this.sliding)
+		return false;
+
+	// Get the index of handle that was used
+	var move_handle = $(ui.handle).index();
+	// Get the other handle
+	var other_handle = null;
+	if (move_handle == 1) other_handle = "start"
+	else if (move_handle == 2) other_handle = "end";
+	
+	// Get range size
+	var range = this.getRange();
+	var start = range[0]; var end = range[1];
+	var range_size = end-start;
+	
+	// Check if outside maximum range
+	if (range_size > this.max_units) {
+		if (other_handle == "start") start = end-this.max_units;
+		if (other_handle == "end") end = start+this.max_units;
+		
+		// Move the other handle closer
+		this.sliding = true;
+		this.setRange(start, end);
+		this.sliding = false;
+	}
+	
+	return true;
+});
 
 // Method to create the display object
 // takes a parent jquery element as input
@@ -84,7 +140,15 @@ MemoryScrollbar.method('create', function(parent) {
 		range = ranges[r];
 		this.addHighlight(range[0], range[1], "data");
 	}
+	
+	// Set a maximum range restriction on scrolling
+	this.jqueryObj().bind("slide", {memoryScrollbar: this},
+		function(event, ui) {
+			event.data.memoryScrollbar.maxRange(event, ui);
+		}
+	);
 });
+
 
 /*******************************************************
 * Memory Display - Memory Unit
@@ -172,9 +236,12 @@ MemoryDisplay.method('create', function(parent) {
 	this.displayUnits();
 
 	// Make units refresh on scroll
-	this.scrollbar.jqueryObj().bind("slidechange", {memoryDisplay: this},
+	this.scrollbar.jqueryObj().bind("slidechange", {memoryDisplay: this, memoryScrollbar: this.scrollbar},
 		function(event, ui) {
-			event.data.memoryDisplay.refreshUnits();
+			var valid = event.data.memoryScrollbar.maxRange(event, ui);
+			// Only display units on a valid scroll state
+			if (valid)
+				event.data.memoryDisplay.refreshUnits();
 		}
 	);
 });
