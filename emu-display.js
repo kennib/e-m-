416482c796c18cf0393b02e61007b68db8038ddd
Kenni <kenni.bawden@gmail.com>
2012-03-31 19:29:10 +1100

    Rearranged js code to seperate object definitions from program logic.

--- emu-display.js
+++ emu-display.js
@@ -239,31 +239,3 @@ var MemoryDisplay = {
 		);
 	}
 }
-
-/*******************************************************
-* Main Display Setup
-********************************************************/
-
-$(document).ready(function() {
-	// Create memory object
-	var mem_size = 64000;
-	memory = new Memory(mem_size);
-
-	// Check 50 random memory location chunks
-	var chunk_size = 600;
-	for (var z=0; z<50; z++) {
-		var x = Math.floor(Math.random()*mem_size);
-		for (var p=0; p<chunk_size; p++) {
-			var unit = memory.getUnit(x+p, true);
-			unit.setValue(z);
-		}
-	}
-
-	// Create display for memory object
-	$.widget("ui.MemoryDisplay", MemoryDisplay);
-	// Create the display on the page
-	$("#memory_block").MemoryDisplay({
-		memory: memory
-	});
-});
-
---
+++ emu-objects.js
@@ -0,0 +1,166 @@
+/*******************************************************
+* Register Object
+* 
+* Has a size (bits) and values
+* Method to get a list of bits in its value
+********************************************************/
+
+function Register(bits) {
+	// Register Properties
+	this.bits = bits;
+	this.value = 0;
+	
+	// Register Methods
+	
+	// Getter for the register's value
+	this.getValue = function() {
+		return this.value;
+	}
+
+	// Setter for the register's value
+	this.setValue = function(value) {
+		this.value = value;
+	}
+	
+	// Method to get bits in Register's value
+	this.getBits = function() {
+		var bits = [];
+		for (var b=0; b<this.bits; b++) {
+			var bit = (this.value >> b) & 1;
+			bits.unshift(bit);
+		}
+		
+		return bits;
+	}
+}
+
+/*******************************************************
+* Label Object
+* 
+* Has a name and a memory address
+* Getters and setters to change the location it labels
+********************************************************/
+
+function Label(name, address) {
+	// Label properties
+	this.name = name;
+	this.address = address;
+	
+	// Label methods
+	
+	// Getter for the Label's address
+	this.getAddress = function() {
+		return this.address;
+	}
+
+	// Setter for the Label's address
+	this.setAddress = function(address) {
+		this.address = address;
+	}
+}
+
+/*******************************************************
+* MemoryUnit Object
+* 
+* Has a value and a memory address
+* Getters and setters for memory's value
+********************************************************/
+
+function MemoryUnit(address) {
+	// MemoryUnit properties
+	this.address = address;
+	this.value = 0;	
+	// Has the value of this element been set?
+	// useful for highlighting
+	this.set = false;
+	
+	// MemoryUnit methods
+	
+	// Getter for value of the MemoryUnit
+	this.getValue = function() {
+		return this.value;
+	}
+
+	// Setter for value of the MemoryUnit
+	this.setValue = function(value) {
+		this.value = value;
+		this.set = true;
+	}
+
+	// Returns the unit's address in hex
+	// pads with leading zeros if given parameter
+	this.addressHex = function(padding) {
+		if (!padding) padding = 0;
+		for (var pad=""; pad.length < padding; pad+="0");
+		
+		var hex = this.address.toString(16).toUpperCase();
+		
+		return (pad+hex).slice(-padding);
+	}
+}
+
+/*******************************************************
+* Memory Object
+* 
+* Stores the memory and its corresponding labels
+* Has a function to access memory at any given address
+********************************************************/
+
+function Memory(size) {
+	// Memory properties
+	this.size = size;
+	this.memoryArray = Array(size);
+	this.labelArray = {};
+
+	
+	// Memory methods
+	
+	// Getter for value of the MemoryUnit
+	// creates new, default memory if it does not exist yet
+	this.getUnit = function(address, create) {
+		var unit = this.memoryArray[address];
+		
+		// If the memory unit doesn't exist
+		// Create a new, default unit
+		if (!unit && create) {
+			unit = new MemoryUnit(address);
+			this.memoryArray[address] = unit;
+		}
+		
+		return unit;
+	}
+	
+	// Returns the ranges of units of memory that have been explicitly set
+	// Format is a list of lists of length 2 containing start and end addresses
+	// useful for the memory display scrollbar
+	this.getUnitRanges = function() {
+		var ranges = [];
+		var range = null;
+		
+		// Loop through each defined unit of memory
+		for (u in this.memoryArray) {
+			u = parseInt(u);
+			
+			// If there is no range, set it to the current Unit
+			if (!range) {
+				range = [u, u];
+			}
+			// Else check if the unit is not adjacent to
+			// the last item in the curren range
+			// or if it has not been explicilty set
+			else if (u != ++range[1] || !this.memoryArray[u].set) {
+				// Store this range
+				ranges.push(range);
+				// Start new range
+				range = [u, u];
+			}
+		}
+		
+		if (range) {
+			range[1]++;
+			ranges.push(range);		
+		}
+		
+		return ranges;
+	}
+}
