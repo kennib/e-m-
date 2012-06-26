22e49efdef7ddc65f222a3a1798756b5de8f35f8
Kenni <kenni.bawden@gmail.com>
2012-06-27 03:30:24 +1000

    Removed random data insertion

--- emu.js
+++ emu.js
@@ -50,17 +50,6 @@ $(document).ready(function() {
 	var memory = mc.memory;
 	var registers = mc.registers;
 
-	// Check 50 random memory location chunks
-	var chunk_size = 600;
-	for (var z=0; z<50; z++) {
-		var x = Math.floor(Math.random()*memory.size);
-		for (var p=0; p<chunk_size; p++) {
-			var unit = memory.getUnit(x+p, true);
-			if (unit)
-				unit.setValue(z);
-		}
-	}
-	
 	// Create display for memory object
 	$.widget("ui.MemoryDisplay", MemoryDisplay);
 	// Create the display on the page
