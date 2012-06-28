de3bfd589044182508fc7b00319635f4ba8df90c
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-06-28 14:32:05 +1000

    Fixed typos.

--- emu-microcontrollers.js
+++ emu-microcontrollers.js
@@ -187,44 +187,44 @@ function Motorola68HC11() {
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["INY"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("Y").value++;
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["INS"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("SP").value++;
 		}
 	);
 	
 	// Decrement operations
 	mc.addMultiAddressOp(opcodes68HC11["DEC"],
-		function(me, memory) {
+		function(mc, memory) {
 			memory[0].value--;
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["DECA"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("A").value--;
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["DECB"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("B").value--;
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["DEX"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("X").value--;
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["DEY"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("Y").value--;
 		}
 	);
 	mc.addMultiAddressOp(opcodes68HC11["DES"],
-		function(me, memory) {
+		function(mc, memory) {
 			mc.registers.getRegister("SP").value--;
 		}
 	);
